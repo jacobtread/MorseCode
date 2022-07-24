59a15fc325b5d2ba0cf5bd5fcb5f2033bc3915a9
Jacobtread <jacobtread@gmail.com>
2022-07-24 14:58:18 +1200

    Fixed missing mappnig for I character

--- src/main/kotlin/com/jacobtread/morse/MorseCode.kt
+++ src/main/kotlin/com/jacobtread/morse/MorseCode.kt
@@ -14,7 +14,7 @@ object MorseCode {
      */
     private val mapping: Map<Char, String> = mapOf(
         'A' to ".-", 'B' to "-...", 'C' to "-.-.", 'D' to "-..", 'E' to ".",
-        'F' to "..-.", 'G' to "--.", 'H' to "....", 'I' to "", 'J' to ".---",
+        'F' to "..-.", 'G' to "--.", 'H' to "....", 'I' to "..", 'J' to ".---",
         'K' to "-.-", 'L' to ".-..", 'M' to "--", 'N' to "-.", 'O' to "---",
         'P' to ".--.", 'Q' to "--.-", 'R' to ".-.", 'S' to "...", 'T' to "-",
         'U' to "..-", 'V' to "...-", 'W' to ".--", 'X' to "-..-", 'Y' to "-.--",
